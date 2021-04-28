187421c431750f6b5522b142960b47a4016ec1d7
A. Alesik <33415083+AAlesik@users.noreply.github.com>
2021-04-28 17:05:19 +0200

    translation

--- app/src/main/java/de/ur/mi/android/laufapp/StatsHelper.java
+++ app/src/main/java/de/ur/mi/android/laufapp/StatsHelper.java
@@ -1,12 +1,12 @@
 package de.ur.mi.android.laufapp;
 
 public class StatsHelper {
-    // Average values for calorie calculation
+    // Durschnittswerte zur Berechnung von Kalorien
     private final static double FAST_JOGGING_KCAL_PER_HOUR = 840;
     private final static double SLOW_JOGGING_KCAL_PER_HOUR = 530;
     private final static double MIN_SPEED_FOR_FAST_JOGGING = 7;
 
-    // Default values for distance, time and pauses
+    // Default-Werte für Distanz, Zeit und Pausen
     private final static double DEFAULT_DISTANCE_IN_KM = 10;
     private final static double DEFAULT_TIME_IN_MIN = 60;
     private final static double DEFAULT_BREAK_TIME = 0;
@@ -16,48 +16,48 @@ public class StatsHelper {
     private double breakTime = DEFAULT_BREAK_TIME;
 
     /**
-     * Sets travelled distance
-     * @param distance distance in kilometers
+     * bestimmt die gelaufene Distanz
+     * @param distance Distanz in Kilometern
      */
     public void setDistance(double distance) {
         this.distance = distance;
     }
 
     /**
-     * Sets time used
-     * @param time time in minutes
+     * bestimmt die gelaufene Zeit
+     * @param time Zeit in Minuten
      */
     public void setTime(double time) {
         this.time = time;
     }
 
     /**
-     * Sets overall break time
-     * @param breakTime break time in minutes
+     * bestimmt die Gesamtpausenzeit
+     * @param breakTime Pausen in Minuten
      */
     public void setBreakTime(double breakTime) {
         this.breakTime = breakTime;
     }
 
     /**
-     * Returns average pace
-     * @return average pace in minutes per kilometer
+     * gibt die Pace (Laufsport) zurück
+     * @return durchschnittliche Pace: min/km (Minuten pro Kilometer)
      */
     public double getPace() {
         return time/distance;
     }
 
     /**
-     * Returns average speed
-     * @return average speed in kilometers per hour
+     * gibt die Durchschnittsgeschwindigkeit zurück
+     * @return Durchschnittsgeschwindigkeit in km/h (Kilometer pro Stunde)
      */
     public double getSpeed() {
         return distance / ((time - breakTime)/60);
     }
 
     /**
-     * Returns estimated calories burned during run
-     * @return estimated kilocalories burned during run
+     * gibt die geschätzen verbrannten Kalorien zurück
+     * @return geschätze Kilokalorien, die während des Laufes verbraucht wurden
      */
     public double getBurnedKilocalories() {
         double speed = getSpeed();
